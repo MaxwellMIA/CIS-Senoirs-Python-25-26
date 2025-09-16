ba080f8679733caa8bdc625c6fb4aa6c8e776af8
Maxwell <cte-berry.gavigan@sllboces.org>
2025-09-16 13:52:57 +0000

    hii

---
+++ printExepressions.py
@@ -0,0 +1,15 @@
+'''
+Class: CIS senoirs 25-26
+Name: Berry Gavigan
+Assignment: printing expressions
+'''
+
+print("Hello, my name is Berry.")
+
+print("My favorite song!")
+print("----------")
+print()
+print("We're the ones who feel insane.")
+print("We're the ones who won't desert you.")
+print("There's nothing sacred in our name.")
+print("All for one we are young aliens")
