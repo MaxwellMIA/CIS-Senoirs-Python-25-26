d062eb21e06421b944c96dfb0b50f91390a694c3
Maxwell <cte-berry.gavigan@sllboces.org>
2025-09-18 13:27:27 +0000

    owen

---
+++ dreamVacation.py
@@ -0,0 +1,18 @@
+'''
+File Name: dreamVacation.py
+Your Name: Berry Gavigan
+Course Name: CIS
+Date: 9/18/25
+'''
+
+location = "Netherlands" 
+cost = "$18000"
+duration = 3
+activities = "swimming"
+companions = "Seth and Owen"
+
+print("My dream location is the" , location)
+print("I will bring" , companions , "as a companion")
+print("The cost for me," , companions , "will be" , cost)
+print("We will be staying at the" , location , "for" , duration , "days")
+print("We will be" , activities , "in the" , location)
---
+++ morePyVars.py
@@ -0,0 +1,36 @@
+'''
+Description: Notes about python varibales
+Name: Berry Gavigan
+Date: 9/18
+'''
+
+print("\n\n\n")
+# variable assigment
+firstName = "Berry"
+lastName = "Gavigan"
+gradeLevel = "12th"
+favoriteSubject = "CIS"
+yearsInSchool = 15
+
+print("My name is " + firstName + " " + lastName + ".")
+print("I am in " + gradeLevel + " grade.")
+print("My favorite class is " + favoriteSubject + ".")
+print("I have been in school for" , yearsInSchool , "years.")
+
+print("\n\n\n")
+
+print("Mathmatical Calculations:")
+
+#storing mathmathial values
+length = 15
+width = 9
+height = 12
+
+#calculate area and volume and store them in vars
+area = length * width
+volume = length * width * height
+
+#print vaules
+print("The area of our structer is:", area)
+print("te volume of our structure is:", volume)
+
