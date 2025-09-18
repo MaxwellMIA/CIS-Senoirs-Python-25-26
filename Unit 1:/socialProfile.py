ff6f9ed37c65b5b5a56f0b1a347f8a50aea14eaa
Maxwell <cte-berry.gavigan@sllboces.org>
2025-09-18 14:32:08 +0000

    ollie

---
+++ Unit 1:/socialProfile.py
@@ -0,0 +1,26 @@
+'''
+File Name: socialProfile.py
+Your Name: Berry Gavigan
+Course Name: CIS
+Date: 9/18/25
+'''
+
+username = "Oleander Dogbane"
+bio = "Taken, 47."
+favorite_quote = "I'll never forget that day."
+followers = "19"
+following = "3"
+posts = "20"
+average_likes_per_post = "8.9"
+engagement_rate = "4.2"
+verified_account = "false"
+private_profile = "true"
+
+print("Username:" , username , "Bio:" , bio)
+print("Quote:" , favorite_quote)
+print("-----")
+print("Followers:" , followers , "Following:" , following , "Posts:" , posts)
+print("-----")
+print("Average likes per post:" , average_likes_per_post , "Engagement rate:" , engagement_rate)
+print("-----")
+print("Verified account:" , verified_account , "Private profile:" , private_profile)
