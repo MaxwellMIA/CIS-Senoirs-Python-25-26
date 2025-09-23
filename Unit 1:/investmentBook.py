7dad69a7db441e537700ae5a39c75bf3e0c654b4
Maxwell <cte-berry.gavigan@sllboces.org>
2025-09-23 14:51:46 +0000

    input

---
+++ Unit 1:/investmentBook.py
@@ -0,0 +1,45 @@
+'''
+Program: investmentBook.py
+Author: Berry Gavgian
+compute an investment report
+
+1. inputs
+    starting investment amount
+    number of years
+    interest rate (int)
+
+2. the report is displated in tabulr form with a header
+
+3. computations and outputs:
+    for each year of the investment
+        compute the interst and add it to the investment
+        print a formatted row of results for that year
+4. the ending investment and intreest you have paid in total are also displayed
+'''
+print("\n\n")
+print("=" * 50)
+print("my investment tracker")
+print("=" * 50)
+# accpet the imputs
+
+startBalance = float(input("Enter the investment amount: "))
+years = int(input("Enter the number of years: "))
+rate = int(input("enter the rqate as a percentage: "))
+
+# convert the rate to a decimal number
+
+rate = rate / 100
+
+# initialize the accumlaotr for the interest
+
+totalIntrest = 0.0
+
+# display the header for the table
+
+
+
+# compute and display the resuts for each year
+
+
+
+# display the totals for the period
