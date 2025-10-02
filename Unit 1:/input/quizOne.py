d635e11231d5bf2ebf80365e461517d9f05f336f
Maxwell <cte-berry.gavigan@sllboces.org>
2025-10-02 11:10:44 +0000

    test0.7

---
+++ Unit 1:/input/quizOne.py
@@ -0,0 +1,58 @@
+'''
+Program: quizOne.py
+Author: Berry Gavigan
+Class: CIS
+Date: 10/2/25
+'''
+
+#1
+
+for count in range(1, 5 +1):
+    print(count)
+print("\n")
+
+#2
+
+for counting in range(3, 8 +1):
+    print(counting)
+print("\n")
+
+#3
+
+for skip in range(0, 11, +2):
+    print(skip)
+print("\n")
+
+#4
+
+for counts in range(10, 0, -1):
+    print(counts)
+print("\n")
+
+#5
+
+numberOne = 5*1
+numberTwo = 5*2
+numberThree = 5*3
+numberFour = 5*4
+numberFive = 5*5
+
+print("5 x 1 =" , numberOne)
+print("5 x 2 =" , numberTwo)
+print("5 x 3 =" , numberThree)
+print("5 x 4 =" , numberFour)
+print("5 x 5 =" , numberFive)
+
+print("\n")
+#6
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are a adult")
+else:
+    print("You are a minor")
+
+print("\n")
+
+#7
