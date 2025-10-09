1f12bbc909dc3a647345624e8bf418e9fdbedc63
Maxwell <cte-berry.gavigan@sllboces.org>
2025-10-09 00:05:45 +0000

    yippie yay

--- pythonVariableNameValidator.py
+++ pythonVariableNameValidator.py
@@ -27,16 +27,23 @@ print()
 variable_name = input("Enter a variable name to validate: ")
 
 # Your validation code goes here
+invalid_start_chara = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+invalid_chara = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}', '~']
 
 # Check each rule and provide appropriate feedback
-#empty string
+# Empty name
 if variable_name == "" or variable_name == " ":
     print ("Invalid, variable name cant be empty.")
+# Python keyword
 elif variable_name in python_keywords:
     print("Invalid, '" , variable_name , "' is a python keyword")
-elif:
+# Starting with numbers
+elif variable_name.startswith(tuple(invalid_start_chara)):
     first_char = variable_name[0]
-    print("test")
+    print("Invaild, variables can not start with a number(s).")
+# Invaild charaters
+elif any(item in variable_name for item in invalid_chara):
+    print("Invaild, you can only use underscores, numbers, and letters in your variable")
+# vaild
 else:
-    print("HI!")
-
+    print("Vaild!")
